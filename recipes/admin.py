7d7df25599520b79f352200f32a30f792d29af4e
GarborSerey <garborfersru@gmail.com>
2022-03-15 17:47:35 +0200

    Добавил в модели категории блюд и блюда, связал их соотношением однин к многим

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from recipes.models import DishCategory, Dish
 
-# Register your models here.
+admin.site.register(DishCategory)
+admin.site.register(Dish)
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class DishCategory(models.Model):
+    """Категория блюда (первое, второе итд)"""
+    dish_category = models.CharField(max_length=150)
+
+    def __str__(self):
+        """Возвращает строковое представление модели"""
+        return self.dish_category
+
+class Dish(models.Model):
+    """Блюдо"""
+    category = models.ForeignKey(DishCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    introduction = models.TextField()
+    ingredients_input = models.TextField()
+    ingredients_num = models.CharField(max_length=3)
+    time_cook = models.CharField(max_length=20)
+    recipe = models.TextField()
+
