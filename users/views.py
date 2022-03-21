f79d720864c80e9630e2884189d04e53b13ceb92
GarborSerey <garborfersru@gmail.com>
2022-03-21 12:00:36 +0300

    поменял язык на русский, эксперементировал с формой регистрации

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpUserForm
 # Проще всего воспользоваться UserCreationForm и представлением на основе класса CreateView,
 # предоставляемыми Django.
 # UserCreationForm - это класс ModelForm для создания нового пользователя, который генерирует требуемые поля,
@@ -12,6 +13,6 @@ from django.views.generic.edit import CreateView
 # Добавляю класс представления
 
 class SignUp(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpUserForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
