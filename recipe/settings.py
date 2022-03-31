761e8ca8e6bb1743a804317055b3cd41ac84b989
GarborSerey <garborfersru@gmail.com>
2022-03-31 10:23:39 +0300

    turn two

--- recipe/settings.py
+++ recipe/settings.py
@@ -129,16 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'recipes/staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'recipe/static'),
+    os.path.join(PROJECT_ROOT, 'recipes/static'), # ВОТ ТУТ СМОТРИ!
 )
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
